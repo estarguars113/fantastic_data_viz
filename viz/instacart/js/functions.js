23f6fbdbfcd40507682db24a6a344ad25a0a5464
Julian Hernández <juliusvernedesign@gmail.com>
2017-12-19 15:27:17 -0500

    Again.

--- viz/instacart/js/functions.js
+++ viz/instacart/js/functions.js
@@ -1,10 +1,36 @@
-d3.csv("db/orders.csv", function(data) {
-  console.log(data);
-  $('.loader').addClass('disabled');
-  $('.dimmer').removeClass('active');
 
-  usingD3();
+
+/*d3.csv("db/orders.csv")
+  .on("progress", function(evt) {
+         console.log("Amount loaded: " + evt.loaded)
+     })
+     .get(function(data) {
+         console.log(data);
+         $('.loader').addClass('disabled');
+         $('.dimmer').removeClass('active');
+           usingD3();
+     });
+*/
+d3.csv('db/products.csv', function(data) {
+    console.log(data);
+    console.log("test_1");
+    $('.loader').addClass('disabled');
+    $('.dimmer').removeClass('active');
 });
+
+d3.csv('db/objects.csv', function(data) {
+    console.log(data);
+    $('.loader').addClass('disabled');
+    $('.dimmer').removeClass('active');
+});
+/**
+ var db_dow = d3.nest()
+      .key(function(d) { return data.order_dow; })
+      .rollup(function(d) {
+        return d3.sum(d, function(g) {return g.value; });})
+      .entries(data);
+ */
+
 
 function usingD3() {
 
@@ -17,6 +43,5 @@ var svg = d3.select('#cart').append('svg')
     .attr('height', height + margin.top + margin.bottom)
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
 
 }
